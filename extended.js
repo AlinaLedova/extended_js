d59763768d28e4426ae0b691170cb8c34edaa64e
Alina_Ledova <alina@friday13.team>
2021-03-12 11:44:10 +0300

    +: added Date.format() method;

--- extended.js
+++ extended.js
@@ -25,3 +25,24 @@ String.Empty = "";
 Number.prototype.toFixedNum = function (fractionDigits) {
     return Number(this.toFixed(fractionDigits));
 };
+
+Date.prototype.format = function (formatString) {
+    let formatTypes = {
+        Y: this.getFullYear(),
+        m: (this.getMonth() + 1 > 9) ? this.getMonth() + 1 : "0" + (this.getMonth() + 1),
+        d: this.getDate(),
+        H: this.getHours(),
+        i: this.getMinutes(),
+        s: this.getSeconds()
+    };
+
+    let dateParts = formatString.split("");
+    let formattedDate = dateParts.map(function (symbol) {
+        if (symbol !== null && symbol !== undefined && formatTypes.hasOwnProperty(symbol))
+            return formatTypes[symbol];
+        else
+            return symbol;
+    });
+
+    return formattedDate.join("");
+}
